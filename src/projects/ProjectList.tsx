1beca44fed6eb647f568ff5bc9fc349b5827ced9
Lynn <qingyang.paul@gmail.com>
2026-08-23 15:46:50 +0800

    display list data

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -6,7 +6,24 @@ interface ProjectListProps {
 }
 
 function ProjectList({projects}: ProjectListProps) {
-    return <pre>{JSON.stringify(projects, null, ' ')}</pre>
+    return (
+        <ul className = "row">
+            {projects.map((project) => (
+                <li key = {project.id} className = "cols-sm"> 
+                    <div className = "card">
+                        <img src={project.imageUrl} alt={project.name} className = "card-img-top" />
+                        <section className = "card-body">
+                            <h5 className = "card-title">
+                                <strong>{project.name}</strong>
+                            </h5>
+                            <p className = "card-text">{project.description}</p>
+                            <p>{project.budget.toLocaleString()}</p>
+                        </section>
+                    </div>
+                </li>
+            ))}
+        </ul> 
+    );
 }
 
 export default ProjectList;
