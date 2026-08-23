e5ac0db0d277ed9689f3f8cd9f9d6ece7c0e9c87
Lynn <qingyang.paul@gmail.com>
2026-08-23 15:22:10 +0800

    passing data to a component

---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,12 @@
+import { Project } from "./Project";
+
+
+interface ProjectListProps {
+  projects: Project[];
+}
+
+function ProjectList({projects}: ProjectListProps) {
+    return <pre>{JSON.stringify(projects, null, ' ')}</pre>
+}
+
+export default ProjectList;
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,14 +1,13 @@
 import { MOCK_PROJECTS } from "./MockProjects";
+import ProjectList from "./ProjectList";
 
 function ProjectsPage(){
     return(
     <>
         <h1> Projects </h1>
-        <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+        <ProjectList projects={MOCK_PROJECTS} />
     </>
-    )
-
-        
+    )   
 };
 
 export default ProjectsPage;
