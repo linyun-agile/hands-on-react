da13e49b6aec7e634f0cc01218e6d1fe5b3a881d
Lynn <qingyang.paul@gmail.com>
2026-08-23 23:50:34 +0800

    routers basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import ProjectsPage from "./projects/ProjectsPage";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router";
+import HomePage from "./home/HomePage";
 
 function App() {
   return (
-    <div className="Container">
-      <ProjectsPage />
-      
-    </div>
-  )
+    
+    <BrowserRouter>
+    <header>
+        <div className="logo">
+
+        </div>
+        <NavLink to="/" className="button rounded" >
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded" >
+          Projects
+        </NavLink>
+    </header>
+      <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
